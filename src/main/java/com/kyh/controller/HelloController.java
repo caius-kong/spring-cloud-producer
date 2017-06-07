7c0a6220bed8c8f463b04e1f49d7f2cafd4931bd
孔昀晖 <1032316751@qq.com>
2017-06-07 17:18:56 +0800

    Eureka client 服务提供者

---
+++ src/main/java/com/kyh/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.kyh.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        String myIP = "";
+        try{
+            myIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "hello "+name+"，this is first messge from " + myIP + ":" + env.getProperty("server.port");
+    }
+}
