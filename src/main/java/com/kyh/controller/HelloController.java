50a3f2a12ca2ef97d66d76e32ef083dd863e77d2
孔昀晖 <1032316751@qq.com>
2017-06-13 11:03:35 +0800

    client refresh - simple 每次修改需要触发：http://localhost:9000/refresh

--- src/main/java/com/kyh/controller/HelloController.java
+++ src/main/java/com/kyh/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.kyh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 
 @RestController
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class HelloController {
     @Autowired
     private Environment env;
